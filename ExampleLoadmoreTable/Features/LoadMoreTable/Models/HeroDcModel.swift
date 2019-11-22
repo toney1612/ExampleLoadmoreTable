102a808752a3a71a583c130209ff43910c968ebf
Damrongdech Haemanee <toney.damrongdate@gmail.com>
2019-11-23 00:18:31 +0700

    first commit

---
+++ ExampleLoadmoreTable/Features/LoadMoreTable/Models/HeroDcModel.swift
@@ -0,0 +1,9 @@
+//
+//  HeroDcModel.swift
+//  ExampleLoadmoreTable
+//
+//  Created by Toney on 22/11/2562 BE.
+//  Copyright © 2562 Toney. All rights reserved.
+//
+
+import Foundation
---
+++ ExampleLoadmoreTable/Features/LoadMoreTable/Views/HeroDCTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HeroDCTableViewCell.swift
+//  ExampleLoadmoreTable
+//
+//  Created by Toney on 22/11/2562 BE.
+//  Copyright © 2562 Toney. All rights reserved.
+//
+
+import UIKit
+
+class HeroDCTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
